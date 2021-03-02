9fd492b5a95f777f2bb39b2b21b4a37f515356f8
nguyenduykhanh159 <duykhanhctb@gmail.com>
2021-03-02 20:49:03 +0700

    first commit

---
+++ Lab01/Example/Example1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examplelab01;
+
+/**
+ *
+ * @author Admin
+ */
+public class Example1 {
+    public static void main(String[] args) {
+        System.out.println("Xin chao \n cac ban!");
+        System.out.println("Hello \t world!");
+    }
+}
---
+++ Lab01/Example/Example2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examplelab01;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Admin
+ */
+public class Example2 {
+     public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World! How are you?");
+        System.exit(0);
+    }
+}
