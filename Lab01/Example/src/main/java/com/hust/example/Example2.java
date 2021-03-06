6b142a22d8ecf869bafb689d8f73a7154c3de100
nguyenduykhanh159 <duykhanhctb@gmail.com>
2021-03-06 22:16:26 +0700

    first commit

---
+++ Lab01/Example/src/main/java/com/hust/example/Example2.java
@@ -0,0 +1,10 @@
+package com.hust.example;
+
+import javax.swing.*;
+
+public class Example2 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World! How are you?");
+        System.exit(0);
+    }
+}
