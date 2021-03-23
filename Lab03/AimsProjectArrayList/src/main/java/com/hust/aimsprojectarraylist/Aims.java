de421531414b3ccb972e8701a7721b42380fc25e
nguyenduykhanh159 <duykhanhng@gmail.com>
2021-03-23 15:34:53 +0700

    create folder lab03

---
+++ Lab03/AimsProjectArrayList/src/main/java/com/hust/aimsprojectarraylist/Aims.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hust.aimsprojectarraylist;
+
+/**
+ *
+ * @author Nguyen Duy Khanh
+ */
+public class Aims {
+    public static void main(String[] args) {
+        Order anOrder = new Order();
+        
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Naruto", "Anime", "Kishimoto", 72, 36.05f);
+        anOrder.addDigitalVideoDisc(dvd4);
+        
+//        System.out.println(anOrder.getItemsOrdered().size());
+        anOrder.removeDigitalVideoDisc(dvd4);
+        anOrder.totalCost();
+    }
+}
